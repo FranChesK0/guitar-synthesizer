82429977a4dc17e449cef01da67737867de998c3
FranChesK0 <khl_doss@mail.ru>
2024-11-29 15:39:45 +0800

    implement measured timeline

--- src/guitar_synth/temporal.py
+++ src/guitar_synth/temporal.py
@@ -1,7 +1,7 @@
 from typing import Self
 from decimal import Decimal
 from fractions import Fraction
-from dataclasses import dataclass
+from dataclasses import field, dataclass
 
 type Numeric = int | float | Decimal | Fraction
 type Hertz = int | float
@@ -39,6 +39,23 @@ class Time:
             case _:
                 raise TypeError(f"Unexpected type '{type(seconds).__name__}' for seconds")
 
+    def __radd__(self, seconds: Numeric | Self) -> "Time":
+        return self + seconds
+
+    def __mul__(self, seconds: Numeric) -> "Time":
+        match seconds:
+            case int() | Decimal():
+                return Time(self.seconds * seconds)
+            case float():
+                return Time(self.seconds * Decimal(str(seconds)))
+            case Fraction():
+                return Time(Fraction.from_decimal(self.seconds) * seconds)
+            case _:
+                raise TypeError(f"Unexpected type '{type(seconds).__name__}' for seconds")
+
+    def __rmul__(self, seconds: Numeric) -> "Time":
+        return self * seconds
+
     def get_num_samples(self, sample_rate: Hertz) -> int:
         return round(self.seconds * round(sample_rate))
 
@@ -49,4 +66,24 @@ class TimeLine:
 
     def __rshift__(self, seconds: Numeric | Time) -> Self:
         self.instant += seconds
+        return self
+
+
+@dataclass
+class MeasuredTimeline(TimeLine):
+    measure: Time = Time(0)
+    last_measure_ended_at: Time = field(init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        if self.measure.seconds > 0 and self.instant.seconds > 0:
+            periods = self.instant.seconds // self.measure.seconds
+            self.last_measure_ended_at = Time(periods * self.measure.seconds)
+        else:
+            self.last_measure_ended_at = Time(0)
+
+    def __next__(self) -> Self:
+        if self.measure.seconds <= 0:
+            raise ValueError("Measure must be greater than 0 seconds")
+        self.last_measure_ended_at += self.measure
+        self.instant = self.last_measure_ended_at
         return self
